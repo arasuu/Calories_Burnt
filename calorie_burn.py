9b56332924478d0be261d7ec0c42e52701809223
arasuu <arasuragu23@gmail.com>
2025-03-27 10:11:40 -0400

    Update calorie_burn.py

--- calorie_burn.py
+++ calorie_burn.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/117fVraFDVzvrrBYQuf14mS9yC56ZJ04p
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import xgboost as xgb
